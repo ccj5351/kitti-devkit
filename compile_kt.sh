21be5eecdd4fdf6890ad0fb003c5df4defb7e58d
Changjiang_Cai <caicj5351@gmail.com>
2019-05-07 14:37:53 -0400

    delete compile_kt.sh, just use CMake for compilation

--- compile_kt.sh
+++
@@ -1,5 +0,0 @@
-cd devkit_stereo_flow_kt12/cpp
-#g++ -O3 -DNDEBUG -o evaluate_stereokt12 evaluate_stereo.cpp -lpng
-cd ../../devkit_scene_flow_kt15/cpp
-g++ -O3 -DNDEBUG -o evaluate_kt15 evaluate_scene_flow.cpp -lpng
-g++ -O3 -DNDEBUG -o get_color_disp getColorDisp.cpp -lpng
